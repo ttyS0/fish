d8e7a56eba468e8a7b61cbd0190d582dd01542c6
Sean Johnson <sean@skj.dev>
2021-08-24 10:39:18 -0500

    simplified exa aliases

--- conf.d/exa.fish
+++ conf.d/exa.fish
@@ -1,38 +1,11 @@
-alias l 'exa'
-alias ll 'exa_git'
-alias la 'exa $EXA_STANDARD_OPTIONS $EXA_LA_OPTIONS'
-alias ld 'exa $EXA_STANDARD_OPTIONS $EXA_LD_OPTIONS'
-alias lg 'exa $EXA_STANDARD_OPTIONS $EXA_LG_OPTIONS'
-alias le 'exa $EXA_STANDARD_OPTIONS $EXA_LE_OPTIONS'
-alias lt 'exa $EXA_STANDARD_OPTIONS $EXA_LT_OPTIONS'
+alias ls 'exa'  # ls
+alias l 'exa -lbF --git'  # list, size, type, git
+alias ll 'exa -lbGF --git'  # long list
+alias llm 'exa -lbGF --git --sort=modified' # long list, modified date sort
+alias la 'exa -lbhHigUmuSa --time-style=long-iso --git --color-scale' # all list
+alias lx 'exa -lbhHigUmuSa@ --time-style=long-iso --git --color-scale'  # all + extended list
 
-function __fish_exa_install --on-event fish-exa_install
-    set -Ux EXA_STANDARD_OPTIONS "--long" "--all" "--group" "--header"
-    set -Ux EXA_LA_OPTIONS "--binary" "--links" "--inode" "--blocks"
-    set -Ux EXA_LD_OPTIONS "--list-dirs"
-    set -Ux EXA_LG_OPTIONS "--git"
-    set -Ux EXA_LE_OPTIONS "--extended"
-    set -Ux EXA_LT_OPTIONS "--tree" "--level"
-end
+ # speciality views
+alias lS 'exa -1' # one column, just names
+alias lt 'exa --tree --level=2' # tree
 
-function __fish_exa_update --on-event fish-exa_update
-    __fish_exa_uninstall
-    __fish_exa_install
-end
-
-function __fish_exa_uninstall --on-event fish-exa_uninstall
-    set --erase EXA_STANDARD_OPTIONS
-    set --erase EXA_LA_OPTIONS
-    set --erase EXA_LD_OPTIONS
-    set --erase EXA_LG_OPTIONS
-    set --erase EXA_LE_OPTIONS
-    set --erase EXA_LT_OPTIONS
-    functions --erase l
-    functions --erase ll
-    functions --erase exa_git
-    functions --erase la
-    functions --erase ld
-    functions --erase lt
-    functions --erase lg
-    functions --erase le
-end
