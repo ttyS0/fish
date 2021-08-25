929ca20ab25815e27abd2d07f8f39ce028cea419
Sean Johnson <sean@skj.dev>
2021-08-25 10:57:28 -0500

    fixed variable test in exip function

--- functions/exip.fish
+++ functions/exip.fish
@@ -1,7 +1,7 @@
 function exip -d "display external IP (v4 & v6)"
   set -l v4 (curl -s ipv4.icanhazip.com)
   set -l v6 (curl -s icanhazip.com)
-  if test v4 != v6
+  if test $v4 != $v6
     echo $v4
     echo $v6
   else
