22077b75e8f1a971ab545a358f642f39630726fd
Sean Johnson <sean@skj.dev>
2021-08-25 10:55:06 -0500

    added more aliases
    added exip function for displaying external ip
    
    Signed-off-by: Sean Johnson <sean@skj.dev>

--- conf.d/aliases.fish
+++ conf.d/aliases.fish
@@ -9,6 +9,9 @@ abbr .. 'cd ..' # up one
 abbr lns 'ln -s' # slightly shorter symlink
 abbr less "less -MNi" # file info/position, line #'s, smart case
 abbr gr grep
+abbr chx 'chmod +x'
+abbr chR 'chmod -R'
+abbr dut 'du -sch'
 
 # macOS specific
 if test (uname | string lower) = "darwin"
@@ -16,6 +19,12 @@ if test (uname | string lower) = "darwin"
   abbr bu 'brew upgrade'
   abbr bs 'brew search'
   abbr bn 'brew info'
+end
+
+# Linux specific
+if test (uname | string lower) = "linux"
+  abbr ai 'sudo apt install'
+  abbr aup 'sudo apt update; and sudo apt upgrade; and sudo apt autoremove'
 end
 
 
@@ -30,4 +39,13 @@ abbr gph 'git push'
 abbr gb 'git branch -a'
 abbr gs 'git status'
 abbr gplS 'git pull --recurse submodules'
+
+# Docker
+abbr d docker
+abbr dp 'docker ps'
+abbr dpa 'docker ps -a'
+abbr db 'docker build'
+abbr dbx 'docker buildx build --platform linux/arm64,linux/amd64'
+abbr dph 'docker push'
+
 
