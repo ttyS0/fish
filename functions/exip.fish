812aa0902df8804585c1aa29729f10ffec73b6de
Sean Johnson <sean@skj.dev>
2021-08-25 10:56:17 -0500

    actually commit exip function
    Signed-off-by: Sean Johnson <sean@skj.dev>

---
+++ functions/exip.fish
@@ -0,0 +1,10 @@
+function exip -d "display external IP (v4 & v6)"
+  set -l v4 (curl -s ipv4.icanhazip.com)
+  set -l v6 (curl -s icanhazip.com)
+  if test v4 != v6
+    echo $v4
+    echo $v6
+  else
+    echo $v4
+  end
+end
